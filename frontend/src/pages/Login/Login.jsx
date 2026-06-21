ff6e6eec3c94b2af76faced61b8906d33b57bc3a
Kyaw San Lwin <128618172+Kyaw-San-Lwin@users.noreply.github.com>
2026-06-21 16:53:38 +0630

    login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,15 @@
+import "./login.css";
+
 function Login() {
   return (
-    <div>
+    <div className="login-container">
       <form>
-        <label htmlFor="username">Username:</label>
+        <image src="../../assets/logo.png" alt="Tun Pipe Shop Logo" className="logo" />
+        <label htmlFor="username">Username</label>
         <br />
         <input type="text" id="username" name="username" required />
         <br />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password</label>
         <br />
         <input type="password" id="password" name="password" required />
         <br /><br/>
